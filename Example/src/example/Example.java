ddafd7f2e5606ad6008222b568645b3894705ca4
BuyanMax <buyanmaxim@gmail.com>
2020-05-19 20:47:53 +0300

    Add Example/

---
+++ Example/src/example/Example.java
@@ -0,0 +1,12 @@
+package example;
+
+/**
+ *
+ * @author Buyan M.D.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Простая программма!");
+    }
+    
+}
